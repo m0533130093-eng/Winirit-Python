f5aeb57cd191c66f7ab2304bd09902e8f623b1ba
m0533130093-eng <m0533130093@gmail.com>
2025-12-14 12:46:06 +0200

    add code to Lesson3_String_List

---
+++ Lesson3_String_List.py
@@ -0,0 +1,10 @@
+
+if __name__ == '__main__':
+    # tar2 class
+    str="python.com"
+    str=str[len(str)-1]+str[:len(str)-1:1]
+    print(str)
+    # tar3 class
+    print("Enter String")
+    str1=input()
+    print(str1[1:len(str1)-1:2])
