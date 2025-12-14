0aede3c5bfb1aad1962d74ef0f2f2532475a3283
m0533130093-eng <m0533130093@gmail.com>
2025-12-14 12:25:54 +0200

    X

---
+++ Lesson2_Operator.py
@@ -0,0 +1,23 @@
+
+if __name__ == '__main__':
+    print("Enter a number")
+    x=int(input())
+    if x%2==0:
+        print("zoogi")
+    else:
+        print("not zoogi")
+    print(x%2==0)
+
+    #HW
+    #tar1
+    print("Enter tow numbers")
+    x1=float(input())
+    x2=float(input())
+    print(" length of a segment: ",(abs(x1)+abs(x2)))
+
+    #tar2
+    print("Enter amount gram")
+    gram=int(input())
+    print("the gram in kilo: ",int(gram/1000) )
+
+
